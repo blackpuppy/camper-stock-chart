faddf2a7cf974e7cecaa05f18d7aaefad153dc4a
Zhu Ming <mingzhu.z+github@gmail.com>
2017-11-28 11:42:32 +0800

    make highstock work

---
+++ app/common/ajax-functions.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var appUrl = window.location.origin;
+var ajaxFunctions = {
+    ready: function ready (fn) {
+        if (typeof fn !== 'function') {
+            return;
+        }
+
+        if (document.readyState === 'complete') {
+            return fn();
+        }
+
+        document.addEventListener('DOMContentLoaded', fn, false);
+    },
+
+    ajaxRequest: function ajaxRequest (method, url, callback, data, onError) {
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === 4) {
+                if (xmlhttp.status === 200) {
+                    callback(xmlhttp.response);
+                } else {
+                    onError(xmlhttp.status, xmlhttp.response);
+                }
+            }
+        };
+
+        xmlhttp.open(method, url, true);
+        xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+        if (data) {
+            // console.log('data not empty, send data = ', data);
+            xmlhttp.send(data);
+        } else {
+            // console.log('data empty, send request only');
+            xmlhttp.send();
+        }
+    }
+};
---
+++ app/controllers/stockController.client.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+    // Your First Charts
+    var myChart = Highcharts.StockChart('container', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+})();
